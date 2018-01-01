7054a64a36b59f3acf3ead57a342df1237952ac7
Rishabh Mishra <mishrarishabh865@gmail.com>
2018-01-01 22:38:07 +0530

    Added example for object assignment

--- src/exampe.test.js
+++ src/exampe.test.js
@@ -1,5 +1,11 @@
-const {add} = require('./example')
+const {add, addObj} = require('./example')
 
 it('should add 2 number', () => {
   expect(add(2, 4)).toEqual(6)
 })
+
+test('object assignment', () => {
+  const data1 = {one: 1};
+  const data2 = {two: 2}
+  expect(addObj(data1, data2)).toEqual({one: 1, two: 2});
+});
--- src/example.js
+++ src/example.js
@@ -1,5 +1,7 @@
 const add = (x, y) => x + y
 
+const addObj = (x, y) => Object.assign({}, x, y)
+
 module.exports = {
-  add
+  add, addObj
 }
