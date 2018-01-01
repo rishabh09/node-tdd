34b000cada5bdd6e34f5004517879a9afdd8c1a9
Rishabh Mishra <mishrarishabh865@gmail.com>
2018-01-01 22:51:49 +0530

    Added test for async function

--- src/exampe.test.js
+++ src/exampe.test.js
@@ -1,11 +1,18 @@
-const {add, addObj} = require('./example')
+const {add, addObj, sqr} = require('./example')
 
 it('should add 2 number', () => {
   expect(add(2, 4)).toEqual(6)
 })
 
-test('object assignment', () => {
+it('object assignment', () => {
   const data1 = {one: 1};
   const data2 = {two: 2}
   expect(addObj(data1, data2)).toEqual({one: 1, two: 2});
 });
+
+it('should handle callback', done => {
+  sqr(3, (res) => {
+    expect(res).toBe(9)
+    done()
+  })
+})
--- src/example.js
+++ src/example.js
@@ -2,6 +2,8 @@ const add = (x, y) => x + y
 
 const addObj = (x, y) => Object.assign({}, x, y)
 
+const sqr = (x, cb) => cb(x * x)
+
 module.exports = {
-  add, addObj
+  add, addObj, sqr
 }
