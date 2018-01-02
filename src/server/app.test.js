bcded0f79a7332003153d59b0ba0044efb126dd7
Rishabh Mishra <mishrarishabh865@gmail.com>
2018-01-02 23:32:32 +0530

    Updated the express test

--- src/server/app.test.js
+++ src/server/app.test.js
@@ -2,7 +2,11 @@ const request = require('supertest')
 const app = require('./app')
 
 describe('Server', () => {
-  it('should return `hello world`', () => {
-    request(app).get('/').expect('Hello World')
+  it('should return `hello world`', done => {
+    request(app).get('/').then(res => {
+      expect(res.statusCode).toBe(200)
+      expect(res.text).toBe('Hello World')
+      done()
+    })
   })
 })
